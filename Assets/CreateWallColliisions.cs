cfe927f5759da44019eaa277fb2b15c7aadb205f
Jake Young <steroidcrazygun@gmail.com>
2019-10-07 13:12:51 +0100

    Collision detection and maze generation

---
+++ Assets/CreateWallColliisions.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateWallColliisions : MonoBehaviour
+{
+
+    #region Private Variables
+    private LineRenderer m_MazeWalls;
+    private EdgeCollider2D m_MazeWallColliders;
+    #endregion
+
+    private void Start()
+    {
+        m_MazeWalls = this.GetComponent<LineRenderer>();
+        m_MazeWallColliders = this.GetComponent<EdgeCollider2D>();
+
+        CreateMazeCollisions();
+    }
+
+    private void CreateMaze()
+    {
+
+    }
+
+    private void CreateMazeCollisions()
+    {
+        int arraySize = m_MazeWalls.positionCount;
+
+        Vector3[] mazeWallPointArrayV3 = new Vector3[arraySize];
+        m_MazeWalls.GetPositions(mazeWallPointArrayV3);
+
+        Vector2[] mazeWallPointArrayV2 = new Vector2[arraySize];
+        for (int i = 0; i < arraySize; i++)
+        {
+            mazeWallPointArrayV2[i] = mazeWallPointArrayV3[i];
+        }
+
+        m_MazeWallColliders.points = mazeWallPointArrayV2;
+    }
+
+    public void GetWallPoints()
+    {
+
+    }
+
+    public void SetWallPoints()
+    {
+
+    }
+    
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    #region Private Variables
+    private float m_HorizontalMovement = 0.0f;
+    private float m_VerticalMovement = 0.0f;
+    private Rigidbody2D m_PlayerRigidBody;
+
+    [SerializeField]
+    private float m_Speed = 2.0f;
+    #endregion
+
+    private void Start()
+    {
+        m_PlayerRigidBody = this.GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        m_HorizontalMovement = Input.GetAxis("Horizontal");
+        m_VerticalMovement = Input.GetAxis("Vertical");
+        this.transform.Translate(new Vector3(m_HorizontalMovement, 0, 0) * Time.deltaTime * m_Speed);
+        this.transform.Translate(new Vector3(0, m_VerticalMovement, 0) * Time.deltaTime * m_Speed);
+    }
+}
